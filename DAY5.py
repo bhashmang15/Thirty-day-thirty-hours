c6350c2180ced16a5b98c0a448c2345310437a9d
Bhashmang Adhvaryu <85794193+bhashmang15@users.noreply.github.com>
2021-06-16 13:35:05 +0530

    Add files via upload

---
+++ DAY5.py
@@ -0,0 +1,18 @@
+my_list=[15,25,47,95,65,12]
+print('My list:\n',my_list)
+my_list.insert(3,'new item= 55')
+print('New item inserted:\n',my_list)
+del my_list[3]
+print('New item removed:\n',my_list)
+lar=max(my_list)
+print('Largest integer of list =',lar)
+sma=min(my_list)
+print('Smallest interger of list = ',sma)
+my_tuple=(1,2,3,4,5)
+print('\n\nMy tuple :\n',my_tuple)
+#Reverse(my_tuple)
+new_tuple=tuple(reversed(my_tuple))
+print('Reversed tuple:\n',new_tuple)
+
+my_list=list(new_tuple)
+print('\n\nMy new list:\n',my_list)
